5308fb95bc0dcd96d63211f66453d82e5e277bf3
Edwin van den Oetelaar (i7 VM) <edwin@oetelaar.com>
2015-06-06 15:46:40 +0200

    update of VS1063 patches. includes AGC fix, louder encoding

--- NewProject1/inc/data_blobs.h
+++ NewProject1/inc/data_blobs.h
@@ -1,7 +1,7 @@
 #ifndef DATA_BLOBS_H_INCLUDED
 #define DATA_BLOBS_H_INCLUDED
 
-#define PLUGIN_SIZE 6334 /* de VS1063 plugin size */
+#define PLUGIN_SIZE 7083 /* de VS1063 plugin size */
 
 
 #endif /* DATA_BLOBS_H_INCLUDED */
